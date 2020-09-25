ac565a641dd1d817b514a946e35108cb223f304e
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-09-25 12:00:02 +0300

    ADD user interface cleaned up and tweet controller added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,38 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("created_at DESC LIMIT 20")
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+      if @tweet.save 
+       redirect_to tweets_path, notice: 'Tweet was successfully updated.' 
+      else
+        str = "Tweet not saved!"
+        new_line = "<br/>"
+        @tweet.errors.full_messages.each do |msg|
+          str << new_line
+          str << msg
+        end
+        respond_to do |format|
+          format.html { redirect_to tweets_path, alert: str}
+        end
+      end
+  end
+
+  def edit
     
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def tweet_params
+    params.permit(:tweet)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+  validates :tweet, presence: true
+  validates :tweet, length: { minimum: 2 }
 end
